6bae5c16b82a2b29519e13616ec0ddb1ccc700db
phumteet <teetphum@odds.team>
2022-06-06 10:59:46 +0700

    [phum]add: dropdown

--- app/src/main/java/com/odds/oddsbooking/booking_form/BookingFormActivity.kt
+++ app/src/main/java/com/odds/oddsbooking/booking_form/BookingFormActivity.kt
@@ -2,6 +2,9 @@ package com.odds.oddsbooking.booking_form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import com.odds.oddsbooking.R
 import com.odds.oddsbooking.databinding.ActivityBookingFormBinding
 
 class BookingFormActivity : AppCompatActivity() {
@@ -11,5 +14,15 @@ class BookingFormActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // get reference to the string array that we just created
+        val rooms = resources.getStringArray(R.array.rooms)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, rooms)
+        // get reference to the autocomplete text view
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.dropdown)
+        // set adapter to the autocomplete tv to the arrayAdapter
+        autocompleteTV.setAdapter(arrayAdapter)
     }
 }
