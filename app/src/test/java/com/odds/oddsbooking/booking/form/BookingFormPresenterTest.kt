1d8f576c6d6a2fb8ab19e394526d067f9654f0c7
sittidetP <sittidet@odds.team>
2022-06-30 19:10:21 +0700

    [jj] test: BookingFormPresenterTest fullName, phoneNumber

--- app/src/test/java/com/odds/oddsbooking/booking/form/BookingFormPresenterTest.kt
+++ app/src/test/java/com/odds/oddsbooking/booking/form/BookingFormPresenterTest.kt
@@ -21,7 +21,6 @@ class BookingFormPresenterTest {
         presenter.attachView(view)
     }
 
-
     @Test
     fun `when input fullName correct should call onValidateNameError & DisablePreviewButton`() {
         val name = "Tar"
@@ -37,6 +36,35 @@ class BookingFormPresenterTest {
         presenter.validateFullName(name)
         Mockito.verify(view, times(0)).onValidateNameSuccess()
         Mockito.verify(view, times(1)).onValidateNameError(R.string.full_name_empty_err)
+        Mockito.verify(view, times(1)).disablePreviewButton()
+    }
+
+    @Test
+    fun `when input email empty should call onValidateEmailError & DisablePreviewButton`() {
+        val email = ""
+        presenter.validateEmail(email)
+        Mockito.verify(view, times(0)).onValidateEmailSuccess()
+        Mockito.verify(view, times(1)).onValidateEmailError(R.string.email_empty_err)
+        Mockito.verify(view, times(1)).disablePreviewButton()
+    }
+
+    //TODO: Email incorrect, correct
+
+    @Test
+    fun `when input phoneNumber empty should call onValidatePhoneNumberError & DisablePreviewButton`() {
+        val phoneNumber = ""
+        presenter.validatePhoneNumber(phoneNumber)
+        Mockito.verify(view, times(0)).onValidatePhoneNumberSuccess()
+        Mockito.verify(view, times(1)).onValidatePhoneNumberError(R.string.phone_number_empty_err)
+        Mockito.verify(view, times(1)).disablePreviewButton()
+    }
+
+    @Test
+    fun `when input phoneNumber incorrect should call onValidatePhoneNumberError & DisablePreviewButton`() {
+        val phoneNumber = "0123456789"
+        presenter.validatePhoneNumber(phoneNumber)
+        Mockito.verify(view, times(0)).onValidatePhoneNumberSuccess()
+        Mockito.verify(view, times(1)).onValidatePhoneNumberError(R.string.phone_number_format_err)
         Mockito.verify(view, times(1)).disablePreviewButton()
     }
 }
