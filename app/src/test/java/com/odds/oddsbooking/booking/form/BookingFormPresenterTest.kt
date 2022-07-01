8d05920d4e1fd90d2b1d9e0f58dc4cd12a124a93
jdaipopo <jdaipopo11@gmail.com>
2022-07-01 09:42:58 +0700

    test: phoneNumber and select room

--- app/src/test/java/com/odds/oddsbooking/booking/form/BookingFormPresenterTest.kt
+++ app/src/test/java/com/odds/oddsbooking/booking/form/BookingFormPresenterTest.kt
@@ -22,9 +22,12 @@ class BookingFormPresenterTest {
     }
 
     @Test
-    fun `when input fullName correct should call onValidateNameError & DisablePreviewButton`() {
-        val name = "Tar"
+    fun `when input fullName correct should call onValidateNameSuccess & DisablePreviewButton`() {
+        //Given
+        val name = "Mola Mola"
+        //When
         presenter.validateFullName(name)
+        //Then
         Mockito.verify(view, times(1)).onValidateNameSuccess()
         Mockito.verify(view, times(0)).onValidateNameError(R.string.full_name_empty_err)
         Mockito.verify(view, times(1)).disablePreviewButton()
@@ -32,8 +35,11 @@ class BookingFormPresenterTest {
 
     @Test
     fun `when input fullName incorrect should call onValidateNameError & DisablePreviewButton`() {
+        //Given
         val name = ""
+        //When
         presenter.validateFullName(name)
+        //Then
         Mockito.verify(view, times(0)).onValidateNameSuccess()
         Mockito.verify(view, times(1)).onValidateNameError(R.string.full_name_empty_err)
         Mockito.verify(view, times(1)).disablePreviewButton()
@@ -41,8 +47,11 @@ class BookingFormPresenterTest {
 
     @Test
     fun `when input email empty should call onValidateEmailError & DisablePreviewButton`() {
+        //Given
         val email = ""
+        //When
         presenter.validateEmail(email)
+        //Then
         Mockito.verify(view, times(0)).onValidateEmailSuccess()
         Mockito.verify(view, times(1)).onValidateEmailError(R.string.email_empty_err)
         Mockito.verify(view, times(1)).disablePreviewButton()
@@ -52,8 +61,11 @@ class BookingFormPresenterTest {
 
     @Test
     fun `when input phoneNumber empty should call onValidatePhoneNumberError & DisablePreviewButton`() {
+        //Given
         val phoneNumber = ""
+        //When
         presenter.validatePhoneNumber(phoneNumber)
+        //Then
         Mockito.verify(view, times(0)).onValidatePhoneNumberSuccess()
         Mockito.verify(view, times(1)).onValidatePhoneNumberError(R.string.phone_number_empty_err)
         Mockito.verify(view, times(1)).disablePreviewButton()
@@ -61,10 +73,49 @@ class BookingFormPresenterTest {
 
     @Test
     fun `when input phoneNumber incorrect should call onValidatePhoneNumberError & DisablePreviewButton`() {
+        //Given
         val phoneNumber = "0123456789"
+        //When
         presenter.validatePhoneNumber(phoneNumber)
+        //Then
         Mockito.verify(view, times(0)).onValidatePhoneNumberSuccess()
         Mockito.verify(view, times(1)).onValidatePhoneNumberError(R.string.phone_number_format_err)
+        Mockito.verify(view, times(1)).disablePreviewButton()
+    }
+
+    @Test
+    fun `when input phoneNumber correct should call onValidatePhoneNumberSuccess & DisablePreviewButton`() {
+        //Given
+        val phoneNumber = "0823456789"
+        //When
+        presenter.validatePhoneNumber(phoneNumber)
+        //Then
+        Mockito.verify(view, times(1)).onValidatePhoneNumberSuccess()
+        Mockito.verify(view, times(0)).onValidatePhoneNumberError(R.string.phone_number_format_err)
+        Mockito.verify(view, times(1)).disablePreviewButton()
+    }
+
+    @Test
+    fun `when do not select room should call onValidateRoomError & DisablePreviewButton`() {
+        //Given
+        val room = ""
+        //When
+        presenter.validateRoom(room)
+        //Then
+        Mockito.verify(view, times(0)).onValidateRoomSuccess()
+        Mockito.verify(view, times(1)).onValidateRoomError(R.string.room_empty_err)
+        Mockito.verify(view, times(1)).disablePreviewButton()
+    }
+
+    @Test
+    fun `when select room correct should call onValidateRoomSuccess & DisablePreviewButton`() {
+        //Given
+        val room = "Neon"
+        //When
+        presenter.validateRoom(room)
+        //Then
+        Mockito.verify(view, times(1)).onValidateRoomSuccess()
+        Mockito.verify(view, times(0)).onValidateRoomError(R.string.room_empty_err)
         Mockito.verify(view, times(1)).disablePreviewButton()
     }
 }
