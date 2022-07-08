37dc6bb2b82917a794f456d820c3ffe8c96c3fd5
sittidetP <sittidet@odds.team>
2022-07-08 10:57:58 +0700

    [jj] edit splash screen #1

--- app/src/main/java/com/odds/oddsbooking/presentations/splash_screen/SplashActivity.kt
+++ app/src/main/java/com/odds/oddsbooking/presentations/splash_screen/SplashActivity.kt
@@ -3,19 +3,30 @@ package com.odds.oddsbooking.presentations.splash_screen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.odds.oddsbooking.R
 import com.odds.oddsbooking.presentations.booking.BookingFormActivity
 import com.odds.oddsbooking.databinding.ActivitySplashBinding
 import kotlinx.coroutines.Dispatchers
 
 class SplashActivity : AppCompatActivity(), SplashPresenter.SplashView {
     private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
-    private val presenter by lazy { SplashPresenter(Dispatchers.Main,500) }
+    private val presenter by lazy { SplashPresenter(Dispatchers.Main,1200) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        setTheme(R.style.SplashScreenTheme)
         setContentView(binding.root)
 
         presenter.attachView(this)
+
+        val nameNTextAnimation = AnimationUtils.loadAnimation(this, R.anim.name_version_fade)
+
+        binding.appName.startAnimation(nameNTextAnimation)
+        binding.appVersion.startAnimation(nameNTextAnimation)
+
+        //TODO: remove delay but still delay
         presenter.splashing()
     }
 
